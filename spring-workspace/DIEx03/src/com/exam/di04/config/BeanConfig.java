e4e7376bb1d924e152118f23e2aebbf08264d0a7
Doyun9710 <doyun1010k@gmail.com>
2023-11-15 15:08:51 +0900

    add DIEx03

--- spring-workspace/DIEx03/src/com/exam/di04/config/BeanConfig.java
+++ spring-workspace/DIEx03/src/com/exam/di04/config/BeanConfig.java
@@ -6,11 +6,18 @@ import com.exam.di04.model1.BoardDAO;
 import com.exam.di04.model2.ListAction;
 
 public class BeanConfig {
-	BoardDAO dao = new BoardDAO();
+	//BoardDAO dao = new BoardDAO();
+	
+	@Bean
+	public BoardDAO boardDAO() {
+		BoardDAO dao = new BoardDAO();
+		return dao;
+	}
 	
 	@Bean
 	public ListAction listAction() {
-		return new ListAction( dao );
+		//return new ListAction( dao );
+		return new ListAction( new BoardDAO() );
 	}
 	
 }
