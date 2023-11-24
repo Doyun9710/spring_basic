b45d14872ac98baf3cc520ec677a65b56dfc1af3
Doyun9710 <doyun1010k@gmail.com>
2023-11-24 12:31:50 +0900

    Edit MyBatisEx02

--- spring-workspace/MyBatisEx02/src/main/java/com/exam/mvc/controller/ConfigController.java
+++ spring-workspace/MyBatisEx02/src/main/java/com/exam/mvc/controller/ConfigController.java
@@ -1,31 +1,28 @@
 package com.exam.mvc.controller;
 
-import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.exam.mvc.model.JdbcDAO;
+import com.exam.mvc.mapper.SqlMapperInter;
 
 @Controller
+@MapperScan( "com.exam.mvc.mapper" )
 public class ConfigController { 
 	
 	@Autowired
-	private SqlSession sqlSession;
-	@Autowired
-	private JdbcDAO dao;
+	private SqlMapperInter mapper;
 
 	@RequestMapping( "/mybatis.do" )
 	public String mybatis() {
 		System.out.println( "mybatis() 호출" );
 		
-		System.out.println( "sqlSession : " + sqlSession );
-		/*
-		String result = sqlSession.selectOne( "selectNow" );
-		System.out.println( "결과 : " + result );
-		*/
-		dao.selectNow();
+		System.out.println( "mapper : " + mapper );
 		
+		String result = mapper.selectNow();
+		System.out.println( "결과 : " + result );
+
 		return "mybatis";
 	}
 	
